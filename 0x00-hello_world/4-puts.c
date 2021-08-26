3dad53522149c705b59196091396140eb9dcf14b
Patrick Annang <patrickannang23@gmail.com>
2021-08-26 09:17:39 -0700

    puts redo ++

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- *main - Prints "\"Programming is like building a multilingual puzzle", followed by anew line
+ *main - Prints "\"Programming is like building a multilingual puzzle"
+ *                  followed bya new line
  *
  *Return: Always 0
  */
