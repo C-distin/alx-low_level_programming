30edeac9c3080426b75c060f1de44e3e13300cfc
C-distin <paannang@st.ug.edu.gh>
2021-09-01 18:12:17 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * add - function to add two integers and return results
+ *
+ * @a: first integer
+ *
+ * @b: second integer
+ *
+ * Return: Results
+ *
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
+
