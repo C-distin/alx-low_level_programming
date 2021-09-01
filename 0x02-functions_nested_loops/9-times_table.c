f8f85d43d323d072da561748d291a75da812cd14
Tricky Work <trickya93@gmail.com>
2021-09-01 14:24:32 +0000

    times table ++

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,18 +9,18 @@ void times_table(void)
 {
 	int num, mult, prod;
 
-	for (num = 0; num < 10; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
 
-		for (mult = 0; mult < 10; mult++)
+		for (mult = 0; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
 
 			prod = num * mult;
 
-			if (prod > 9)
+			if (prod <= 9)
 			{
 				_putchar(' ');
 			}
