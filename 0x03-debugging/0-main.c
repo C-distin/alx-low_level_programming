a8736ffada1b0ede2937c5b038fe31bbad8a3bd0
C-distin <paannang@st.ug.edu.gh>
2021-09-02 13:45:30 -0700

    main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,21 +7,6 @@
 
 int main(void)
 {
-  int i;
-
-  i = 0;
-  positive_or_negative(i);
-
-  return (0);
-}#include "main.h"
-
-/**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-
-int main(void)
-{
 	int i;
 
 	i = 0;
@@ -29,3 +14,4 @@ int main(void)
 
 	return (0);
 }
+
