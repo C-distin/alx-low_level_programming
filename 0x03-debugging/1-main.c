2b62087d781eb60e4de480c218c45170f1c485e3
C-distin <paannang@st.ug.edu.gh>
2021-09-02 19:44:02 +0000

    infinity loop

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinity loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int 1;
+
+	printf("Infinity loop incoming :(\n");
+
+	i = 0;
+
+	/**
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+
+	printf("Infinity loop avoided! \\o/\n");
+
+	return (0);
+}
+
