e3bde208d41a9a9381a3c4c0d9698cfc67cf01b8
C-distin <paannang@st.edu.gh>
2021-09-17 13:49:21 +0000

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - 
+ *
+ * @argc:
+ * @argv:
+ * Return:
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
