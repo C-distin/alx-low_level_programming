41c600f52d8f7a27cc7bb061364d0051c15c0fb8
C-distin <paannang@st.ug.edu.gh>
2021-09-17 13:20:56 +0000

    my name is III

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,14 +3,14 @@
 
 /**
  * main - program that prints its name, followed by a new line
- * 
+ *
  * @argc: the number of command line arguments
  * @argv: the array containing the program command line arguments
- * 
+ *
  * Return: Always 0
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	(void) argc;
 
