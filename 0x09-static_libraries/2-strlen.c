c8573d373336235e627da4df0dbd58d66f0e817d
C-distin <paannang@st.ug.edu.gh>
2021-09-20 14:57:29 +0000

    static library

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strlen - find lrngth of string
+* @s: string size to be calculated
+* Return: string length
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
+
