d5aad7fcb51e821ebe830b2844b3f40e5bc79c26
C-distin <paannang@st.ug.edu.gh>
2021-09-24 14:37:33 +0000

    string and concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,50 +1,51 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stddef.h>
 
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
+ * string_nconcat - concatenate two strings
+ * @s1: the first string
+ * @s2: the second string
+ * @n: the maximum number of bytes to use from s2
  *
- * Return: integer length of string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (*s++)
-		i++;
-	return (i);
-}
-
-/**
- * string_nconcat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * @n: copy first n bytes of s2
+ * Description: Copy s1 followed by the first n bytes of s2 into dynamically
+ * allocated memory, and append a terminating null-byte. If n is greater than
+ * or equal to the length of the string s2, use only the bytes from s2 up to
+ * it's terminating null-byte. Treat NULL as an an empty string.
  *
- * Return: void
+ * Return: If memory allocation fails, return NULL. Otherwise, return a pointer
+ * to the first character of the new string.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s, *r;
-	unsigned int l1, l2;
+	char *cat;
+	unsigned int s1_len = 0;
+	unsigned int s2_len = 0;
 
-	if (!s1)
-		s1 = "";
-	if (!s2)
-		s2 = "";
-	l1 = _strlen(s1);
-	l2 = _strlen(s2);
+	if (s1)
+	{
+		while (s1[s1_len])
+			++s1_len;
+	}
 
-	l2 = l2 > n ? n : l2;
-	r = s = malloc(l1 + l2 + 1);
-	if (!s)
+	if (s2)
+	{
+		while (s2_len < n && s2[s2_len])
+			++s2_len;
+	}
+
+	cat = malloc(sizeof(char) * s1_len + s2_len + 1);
+	if (!cat)
 		return (NULL);
-	while (*s1)
-		*s++ = *s1++;
-	while (l2--)
-		*s++ = *s2++;
-	*s = '\0';
-	return (r);
+
+	for (n = 0; n < s1_len; ++n, ++cat)
+		*cat = s1[n];
+
+	for (n = 0; n < s2_len; ++n, ++cat)
+		*cat = s2[n];
+
+	*cat = '\0';
+
+	return (cat - s1_len - s2_len);
 }
 
