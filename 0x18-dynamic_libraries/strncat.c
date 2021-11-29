b36246093d066ae6f7a71ca1423f2fa31178641a
C-distin <paannang@st.ug.edu.gh>
2021-11-29 13:44:02 +0000

    update files II

--- 0x18-dynamic_libraries/strncat.c
+++ 0x18-dynamic_libraries/strncat.c
@@ -4,16 +4,17 @@
  * _strncat - concatenate two strings
  * @dest: destination
  * @src: source
+ * @n: number of bytes to copy
  * 
  * Return: pointer to dest
  */
-char *_strncat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0, j = 0;
 
 	while (dest[i] != '\0')
 		i++;
-	while (src[j] != '\0')
+	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
 		i++;
@@ -22,3 +23,4 @@ char *_strncat(char *dest, char *src)
 	dest[i] = '\0';
 	return (dest);
 }
+
