07541a47a6c9a40fda6a49236e0bd278889d0086
C-distin <paannang@st.ug.edu.gh>
2021-12-19 12:24:27 +0000

    Initial commit

---
+++ 0x1A-hash_tables/hash_tables.h
@@ -0,0 +1,13 @@
+#ifndef __HASH_TABLES_H__
+#define __HASH_TABLES_H__
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#define HASH_TABLE_SIZE 100
+#define MAX_NAME 256
+
+#endif /* __HASH_TABLES_H__ */
